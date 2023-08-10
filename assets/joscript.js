0937c669a500677c5ab2d145b832b8219542f0ac
K1tanaK0mbat <kkeayouth@gmail.com>
2023-08-09 20:21:01 -0500

    Journal Page

---
+++ assets/joscript.js
@@ -0,0 +1,44 @@
+var formEl = $('#journal-form');
+var moodInputEl = $('#mood-name');
+var entryInputEl = $('#entry');
+var entryListEl = $('#entry-list');
+
+var printEntries = function (name, entry) {
+  var listEl = $('<li>');
+  var listEnry = name.concat(' on ', entry);
+  listEl.addClass('list-group-item').text(listEntry);
+  listEl.appendTo(enryListEl);
+};
+
+var moodFormSubmit = function (event) {
+  event.preventDefault();
+
+  var moodInput = moodInputEl.val();
+  var entryInput = entryInputEl.val();
+
+
+  printSkills(moodInput, entryInput);
+
+  // resets form
+  moodInputEl.val('');
+  entryInputEl.val('');
+};
+
+formEl.on('submit', moodFormSubmit);
+
+// Autocomplete widget
+$(function () {
+  var moodColors = [
+    'Pink',
+    'Blue',
+    'Green',
+    'Yellow',
+    'Orange',
+    'Red',
+  ];
+  $('#mood-name').autocomplete({
+    source: moodColors,
+  });
+});
+
+
