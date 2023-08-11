9a95e8adfc053858d247ef97e1922e915a0e5ed7
K1tanaK0mbat <kkeayouth@gmail.com>
2023-08-10 21:48:11 -0500

    fixed storage code

--- assets/joscript.js
+++ assets/joscript.js
@@ -1,36 +1,31 @@
 
-var formEl = document.querySelector('#journal-form');
-var moodInputEl = document.querySelector$('#mood-name');
-var entryInputEl = document.querySelector$('#entry');
-var entryListEl = document.querySelector$('#entry-list');
-
-var entries=[]
+var myForm = document.querySelector('#journal-form');
+var moodInput = document.querySelector('#mood-name');
+var entryInput = document.querySelector('#entry-text');
+var entryList = document.querySelector('#entry-list');
+var entries =[];
+var saveEl=document.querySelector("#saveBtn");
 
 function printEntries () {
-  entryListEl.innerHTML="";
-  entryInputEl.textContent=entries.length;
-  for (var i = 0; i < todos.length; i++) {
-    var entry = entries[i];
+     entryList.innerHTML="";
+    for (var i = 0; i < entries.length; i++) {
 
+  var entry=entries[i]
     var li = document.createElement("li");
     li.textContent = entry;
+    entryList.appendChild(li);
     li.setAttribute("data-index", i);
-
-    entryListEl.appendChild(li);
-  }
-  
     }
+  }
 
 
 
 function init() {
     var storedEntry = JSON.parse(localStorage.getItem("entries"));
-  
     
     if (storedEntry !== null) {
       entries = storedEntry;
     }
-
     printEntries();
   }
   
@@ -40,16 +35,16 @@ function init() {
   }
   
  
-  formEl.addEventListener("submit", function(event) {
+  saveEl.addEventListener("click", function(event) {
     event.preventDefault();
   
-    var entryText = formEl.value.trim();
+    var entryText = entryInput.value.trim();
   
     if (entryText === "") {
       return;
     }
     entries.push(entryText);
-    formEl.value = "";
+    entryInput.value = "";
   
 
     storeEntry();
