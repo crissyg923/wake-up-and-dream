b183dcff0bf388f09014f176224ee15755adcef0
K1tanaK0mbat <kkeayouth@gmail.com>
2023-08-14 21:56:10 -0500

    storage saved locally

--- assets/joscript.js
+++ assets/joscript.js
@@ -1,54 +1,20 @@
-var myForm = document.querySelector('#journal-form');
+const myForm = document.querySelector('#journal-form');
 const entryInput = document.querySelector('#entry-text');
 const dateInput = document.querySelector('#date-today');
-var entryList = document.querySelector('#entry-list');
-var entries =[];
+
 var saveEl=document.querySelector("#saveBtn");
 var moodColor=document.querySelector("#color-type")
-
-function printEntries () {
-     entryList.innerHTML="";
-    for (var i = 0; i < entries.length; i++) {
-      var entry=entries[i];
-     
-    var li = document.createElement("li");
-
-    li.textContent = entry;
- //var myMood=document.createElement("h4");
- //myMood.textContent=moodColor.("p");
-   // li.appendChild(myMood)
-
-    entryList.appendChild(li);
+var dreamEl=document.querySelector("#dreamBtn");
 
 
-    var date=document.createElement("p")
-    date.textContent=dateInput.value;
-    li.appendChild(date);
-
-    li.setAttribute("data-index", i);
-    
-    }
-  }
 
 
-function init() {
-    var storedEntry = JSON.parse(localStorage.getItem("entries"));
-    
-    if (storedEntry !== null) {
-      entries = storedEntry;
-    }
-    printEntries();
-  }
-  
-  function storeEntry() {
-   
-    localStorage.setItem("entries", JSON.stringify(entries));
-  }
+
   
  
-  saveEl.addEventListener("click", function(event) {
+  saveEl.addEventListener("submit", function(event) {
     event.preventDefault();
-    
+   
 
     var entryText = entryInput.value.trim();
      
@@ -56,24 +22,24 @@ function init() {
     return;
     }
 
-  var myEntry=entryText+dateText;
-    entries.push(myEntry);
+ getEntries();
+    entries.push(entryText);
     entryInput.value = "";
   
 
     storeEntry();
-    printEntries();
+
   });
   
 
-  init()
+  
 
  
 
 
 var getParameters= function (){
   searchParams=document.location.search.split('&');
-  var moodToday=searchParams[1].split('=').pop();
+  var moodToday=searchParams[1]?.split('=').pop();
   var moodColor=document.getElementById('color-type');
 var moodColorEl=document.createElement('p');
   moodColorEl.innerHTML=moodToday
---
+++ assets/shared.js
@@ -0,0 +1,41 @@
+var entryList = document.querySelector('#entry-list');
+var entries = [];
+function getEntries() {
+    var storedEntry = JSON.parse(localStorage.getItem("entries"));
+
+    if (storedEntry !== null) {
+        entries = storedEntry;
+    }
+}
+function storeEntry() {
+    localStorage.setItem("entries", JSON.stringify(entries));
+}
+function printEntries() {
+    entryList.innerHTML = "";
+    for (var i = 0; i < entries.length; i++) {
+        var entry = entries[i];
+
+        var li = document.createElement("li");
+
+        li.textContent = entry;
+        //var myMood=document.createElement("h4");
+        //myMood.textContent=moodColor.("p");
+        // li.appendChild(myMood)
+
+        entryList.appendChild(li);
+
+
+        var date = document.createElement("p")
+        date.textContent = dateInput.value;
+        li.appendChild(date);
+
+        li.setAttribute("data-index", i);
+
+    }
+}
+
+function init() {
+    getEntries();
+    printEntries();
+}
+
