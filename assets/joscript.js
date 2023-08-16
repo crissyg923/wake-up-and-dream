185a36e33a90a6a79818c1dd645b83d8bc53c770
Janell Smith <119452949+jellobear27@users.noreply.github.com>
2023-08-16 15:28:30 -0400

    fixed references causing errors

--- assets/joscript.js
+++ assets/joscript.js
@@ -1,58 +1,50 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var entryTextArea = document.querySelector('#entry-text');
-  
+document.addEventListener("DOMContentLoaded", function () {
+  var entryTextArea = document.querySelector("#entry-text");
+
   var entries = [];
-  
+
   function getEntries() {
     var storedEntries = JSON.parse(localStorage.getItem("entries"));
-  
+
     if (storedEntries !== null) {
       entries = storedEntries;
     }
   }
-  
+
   function storeEntry(entry) {
     entries.push(entry);
     localStorage.setItem("entries", JSON.stringify(entries));
   }
-  
+
   function init() {
     getEntries();
   }
-  
-  var form = document.querySelector('form');
-  form.addEventListener('submit', function(e) {
+
+  var form = document.querySelector("form");
+  form.addEventListener("submit", function (e) {
     e.preventDefault();
-    
+
     var newEntry = entryTextArea.value.trim();
-    
+
     if (newEntry !== "") {
       storeEntry(newEntry);
-      entryTextArea.value = ""; 
+      entryTextArea.value = "";
     }
-    
-    
-    
-   
   });
-  
 
   init();
 });
 
-
-
-
-
-var getParameters= function (){
-  searchParams=document.location.search.split('&');
-  var moodToday=searchParams[1].split('=').pop();
-  var moodColor=document.getElementById('color-type');
-var moodColorEl=document.createElement('p');
-  moodColorEl.innerHTML=moodToday
-  moodColor.appendChild(moodColorEl);
-}
-
+var getParameters = function () {
+  if (document.location.search.length !== 0) {
+    searchParams = document.location.search.split("&");
+    var moodToday = searchParams[1].split("=").pop();
+    var moodColor = document.getElementById("color-type");
+    var moodColorEl = document.createElement("p");
+    moodColorEl.innerHTML = moodToday;
+    moodColor.appendChild(moodColorEl);
+  }
+};
 
 getParameters();
 
--- assets/shared.js
+++ assets/shared.js
@@ -17,40 +17,43 @@ function storeEntry(entry) {
     localStorage.setItem("entries", JSON.stringify(entries));
 }
 
-function printEntries() {
-    entryList.innerHTML = "";
-    for (var i = 0; i < entries.length; i++) {
-        var entry = entries[i];
+// function printEntries() {
+//     entryList.innerHTML = "";
+//     for (var i = 0; i < entries.length; i++) {
+//         var entry = entries[i];
 
-        var li = document.createElement("li");
-        li.textContent = entry;
+//         var li = document.createElement("li");
+//         li.textContent = entry;
 
-        entryList.appendChild(li);
-    }
-}
+//         entryList.appendChild(li);
+//     }
+// }
 
 function init() {
     getEntries();
-    printEntries();
+    // printEntries();
 }
 
 // Event listener for the form submission
 var form = document.querySelector('form');
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    var newEntry = entryTextArea.value; // Get the value of the textarea
-    
-    if (newEntry.trim() !== "") { // Only store non-empty entries
-        storeEntry(newEntry);
-        printEntries();
-        entryTextArea.value = ""; // Clear the textarea after storing the entry
-    }
-    
-    // You can add any other actions you want here
-    
-    window.location.href = "Entry.html";
-});
+if (form) {
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        
+        var newEntry = entryTextArea.value; // Get the value of the textarea
+        
+        if (newEntry.trim() !== "") { // Only store non-empty entries
+            storeEntry(newEntry);
+            printEntries();
+            entryTextArea.value = ""; // Clear the textarea after storing the entry
+        }
+        
+        // You can add any other actions you want here
+        
+        window.location.href = "Entry.html";
+    });
+}
+
 
 // Call the initialization function
 init();
